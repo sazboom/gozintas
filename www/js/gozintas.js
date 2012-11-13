91f4c3276bec4b76c5c3e8b7b5a6b832df80ff19
Scott Goci <scottjg@gmail.com>
2012-11-12 23:04:12 -0500

    2.2 working on my computer finally

---
+++ www/js/gozintas.js
@@ -0,0 +1,51 @@
+var gozintas = {
+	billModifier : {
+		wine : false,
+		reductions : false,
+		extras : false
+	},
+	billPath : '',
+	splitBy : '',
+	splitBillPath : function() {
+		this.billPath = 'split-bill'
+	},
+	determineTipPath :  function() {
+		this.billPath = 'determine-tip'
+	},
+	isOnSplitBillPath : function() {
+		return this.billPath == 'split-bill'
+	},
+	isOnDetermineTipPath : function(){
+		return this.billpath == 'determine-tip'
+	},
+	splitByIndividual : function() {
+		this.splitBy = 'individual'
+	},
+	splitByGroup : function() {
+		this.splitBy = 'group'
+	},
+	isSplitingByIndividual : function(){
+		return this.splitBy == 'individual'
+	},
+	isSplitingByGroup : function(){
+		return this.splitPath == 'group'
+	},
+	showSplitTipButtons : function(){
+	    if(this.isOnSplitBillPath()){
+	        showButton = ".links-split";
+	        hideButton = ".links-tip";
+	    }else{
+	        showButton = ".links-tip";
+	        hideButton = ".links-split";
+	    }
+	    $(showButton).show();
+	    $(hideButton).hide();
+	},
+	showPageFourButton: function(){
+        if(gozintas.isSplitingByIndividual()){
+            $("#next_button_page4").attr("href", "#fiveb");
+        }else{
+            $("#next_button_page4").attr("href", "#five")
+        }
+	}
+}
