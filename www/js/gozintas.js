e755cfe1b0ec799f37e59a62f2d83da6c9b56df8
Scott Goci <scottjg@gmail.com>
2012-11-19 02:58:18 -0500

    Bunch of additions to gozintas

--- www/js/gozintas.js
+++ www/js/gozintas.js
@@ -10,6 +10,23 @@ var gozintas = {
 	taxAmount : '',
 	totalAmount : '',
 	peopleInParty : '',
+	tipAmount : '0.15',
+	validatePrice: function(price){
+		var re = /^[0-9]+(\.[0-9]{2})?$/;
+		return re.test(price);
+	},
+	calculateTip: function(){
+		return +this.tipAmount * (+this.foodAmount + +this.taxAmount)
+	},
+	calculateTipIndividual: function(){
+		return this.calculateTip()/(+this.peopleInParty)
+	},
+	calculateTotal: function(){
+		return (+this.foodAmount + +this.taxAmount + this.calculateTip())
+	},
+	calculateTotalIndividual: function(){
+		return this.calculateTotal()/(+this.peopleInParty)
+	},
 	splitBillPath : function() {
 		this.billPath = 'split-bill'
 	},
@@ -40,6 +57,9 @@ var gozintas = {
 	storePeopleInParty: function(value){
 		this.peopleInParty = value
 	},
+	storeTipAmount: function(value){
+		this.tipAmount = value;
+	},
 	isSplitingByIndividual : function(){
 		return this.splitBy == 'individual'
 	},
@@ -62,6 +82,18 @@ var gozintas = {
             $("#next_button_page4").attr("href", "#fiveb");
         }else{
             $("#next_button_page4").attr("href", "#five")
+        }
+	},
+	showPageFiveButtons: function(){
+        if(!gozintas.billModifier.wine){
+            $(".page5.wine_tip").hide();
+            $(".page5.wine").hide();
+        }
+        if(!gozintas.billModifier.reductions){
+            $(".page5.reductions").hide();
+        }
+        if(!gozintas.billModifier.extras){
+            $(".page5.extras").hide();
         }
 	}
 }
