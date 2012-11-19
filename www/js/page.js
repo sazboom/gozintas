f56224c1518d93c93c990f9cdb5cb6e2241a5155
Aaron Worsham <aaronworsham@gmail.com>
2012-11-19 14:14:08 -0500

    log total bill and total tax

--- www/js/page.js
+++ www/js/page.js
@@ -4,9 +4,11 @@ var Page = {
 		page2: function() {
 			$('#total_amount_input').on('change', function() {
 				Gozintas.totalBill = parseFloat($(this).val())
+				console.log(Gozintas.totalBill)
 			})
 			$('#total_tax_amount_input').on('change', function() {
 				Gozintas.totalTax = parseFloat($(this).val())
+				console.log(Gozintas.totalTax)
 			})
 
 		}
